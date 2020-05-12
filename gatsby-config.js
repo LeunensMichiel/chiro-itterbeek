55a518bbb8799cd8b5ae169e61b37ef2b028af2e
Michiel Leunens <michiel.leunens@gmail.com>
2020-05-12 18:35:32 +0200

    Initial environtment configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,10 +7,6 @@ const contentfulConfig = {
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
 };
 
-// if you want to use the preview API please define
-// CONTENTFUL_HOST in your environment config
-// the `host` property should map to `preview.contentful.com`
-// https://www.contentful.com/developers/docs/references/content-preview-api/#/reference/spaces/space/get-a-space/console/js
 if (process.env.CONTENTFUL_HOST) {
   contentfulConfig.host = process.env.CONTENTFUL_HOST;
 }
@@ -25,17 +21,51 @@ if (!spaceId || !accessToken) {
 
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Contentful starter",
+    title: "Chiro Itterbeek",
+    description: `Chiro Itterbeek is een jeugdbeweging gesitueerd in Dilbeek, rond Brussel. Chiro Itterbeek verwelkomt alle leeftijden tussen 6 en 18 jaar, om er samen een leuke zondagnamiddag van te maken!`,
+    author: `@leunesmedia`,
+    siteUrl: `https://chiro-itterbeek.be`,
+    keywords: [
+      `chiro`,
+      `itterbeek`,
+      `sint-anna-pede`,
+      `dilbeek`,
+      `jeugd`,
+      `jeugdbeweging`,
+      `jokonta`,
+      `allegro`,
+    ],
   },
-  pathPrefix: "/gatsby-contentful-starter",
   plugins: [
     "gatsby-transformer-remark",
     "gatsby-transformer-sharp",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sharp",
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Chiro Itterbeek`,
+        short_name: `Chiro Itterbeek`,
+        description: `Chiro Itterbeek is een jeugdbeweging gesitueerd in Dilbeek, rond Brussel. Chiro Itterbeek verwelkomt alle leeftijden tussen 6 en 18 jaar, om er samen een leuke zondagnamiddag van te maken!`,
+        start_url: `/`,
+        background_color: `#272728`,
+        theme_color: `#E1143C`,
+        display: `browser`,
+        icon: `src/images/favicon.png`,
+      },
+    },
+    {
       resolve: "gatsby-source-contentful",
       options: contentfulConfig,
     },
+    `gatsby-plugin-emotion`,
+    `gatsby-plugin-sitemap`,
   ],
 };
