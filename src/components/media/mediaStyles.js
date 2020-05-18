4e7106dbd4fb32f1acaebed1d8fafb71b82dd857
Michiel Leunens <michiel.leunens@gmail.com>
2020-05-18 16:21:56 +0200

    more Media styles

--- src/components/media/mediaStyles.js
+++ src/components/media/mediaStyles.js
@@ -3,6 +3,7 @@ import styled from "@emotion/styled"
 export const HighlightedWrapper = styled.section`
   grid-column: 6 / span 8;
   display: flex;
+  height: 315px;
   border: 1px solid ${p => p.theme.colors.border};
   border-radius: ${p => p.theme.radii.borderRadius};
   margin-bottom: ${p => p.theme.space[8]}px;
@@ -54,6 +55,60 @@ export const HighlightedWrapper = styled.section`
           p.gender === 1
             ? p.theme.colors.jokontaLight
             : p.theme.colors.allegroLight};
+      }
+    }
+    small {
+      display: block;
+      font-size: ${p => p.theme.fontSizes[0]}px;
+      color: ${p => p.theme.colors.invisibleText};
+    }
+  }
+`
+
+export const MoreMedia = styled.nav`
+  grid-column: 2 / span 3;
+  display: flex;
+  flex-direction: column;
+  height: 315px;
+  border: 1px solid ${p => p.theme.colors.border};
+  border-radius: ${p => p.theme.radii.borderRadius};
+  margin-bottom: ${p => p.theme.space[8]}px;
+  background-color: ${p => p.theme.colors.backgroundMuted2};
+  transition: border 0.4s cubic-bezier(0.87, 0, 0.13, 1),
+    background-color 0.4s cubic-bezier(0.87, 0, 0.13, 1);
+
+  h2 {
+    align-self: flex-start;
+    display: block;
+    color: ${p => p.theme.colors.styledHeaderText};
+    padding: ${p => p.theme.space[3]}px ${p => p.theme.space[4]}px;
+    margin-top: 0;
+    margin-bottom: ${p => p.theme.space[4]}px;
+    background: ${p =>
+      p.gender === 1 ? p.theme.colors.jokonta : p.theme.colors.allegro};
+    text-transform: uppercase;
+    font-size: ${p => p.theme.fontSizes[2]}px;
+    border-radius: ${p => p.theme.radii.borderRadius};
+    transition: background-color 0.4s cubic-bezier(0.87, 0, 0.13, 1);
+  }
+  .media__links {
+    padding: ${p => p.theme.space[3]}px ${p => p.theme.space[4]}px;
+    a {
+      margin-bottom: ${p => p.theme.space[3]}px;
+      display: flex;
+      align-items: center;
+      color: ${p => p.theme.colors.asideText};
+      text-decoration: underline;
+      font-size: ${p => p.theme.fontSizes[2]}px;
+      font-family: inherit;
+      line-height: 1;
+      transition: color 0.4s cubic-bezier(0.87, 0, 0.13, 1);
+      svg {
+        fill: ${p => p.theme.colors.asideText};
+        margin-top: 4px;
+        margin-left: ${p => p.theme.space[1]}px;
+        height: 11px;
+        transition: fill 0.4s cubic-bezier(0.87, 0, 0.13, 1);
       }
     }
     small {
---
+++ src/components/media/more.js
@@ -0,0 +1,39 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui"
+import { useContext } from "react"
+import { MoreMedia } from "./mediaStyles"
+import { Link } from "gatsby"
+import { GenderContext } from "../../context/GenderContext"
+
+import Chevron from "../../assets/icons/chevron.svg"
+export const More = () => {
+  const { genderState } = useContext(GenderContext)
+
+  return (
+    <MoreMedia gender={genderState.gender}>
+      <h2>Meer Media!</h2>
+      <div className="media__links">
+        <Link>
+          <span>Bekijk alle boekjes</span>
+          <Chevron />
+        </Link>
+        <Link>
+          <span>Bekijk alle foto's</span>
+          <Chevron />
+        </Link>
+        <Link>
+          <span>Bekijk alle filmpjes</span>
+          <Chevron />
+        </Link>
+        <Link>
+          <span>Overige documenten</span>
+          <Chevron />
+        </Link>
+        <small>
+          Wil je meer up to date blijven? Bezoek dan zeker ook onze instagram en
+          facebook!
+        </small>
+      </div>
+    </MoreMedia>
+  )
+}
