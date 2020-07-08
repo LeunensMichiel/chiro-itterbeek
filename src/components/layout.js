de965b9698e1806573fd3e9f3211daedd4d794e2
Michiel Leunens <michiel.leunens@gmail.com>
2020-07-08 14:02:16 +0200

    moved form to layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -61,6 +61,36 @@ const Layout = ({ children }) => {
         </main>
       )}
       {hasMounted && <Footer />}
+      <form
+        name="Contact Allegro"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        hidden
+      >
+        <input type="hidden" name="form-name" value="Contact Allegro" />
+        <input type="hidden" name="bot-field" />
+        <input type="hidden" name="chiro" value="Allegro" />
+        <input type="text" name="naam" />
+        <input type="email" name="email" />
+        <input type="tel" name="telefoon" />
+        <input type="text" name="onderwerp" />
+        <textarea name="bericht"></textarea>
+      </form>
+      <form
+        name="Contact Jokonta"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        hidden
+      >
+        <input type="hidden" name="form-name" value="Contact Jokonta" />
+        <input type="hidden" name="bot-field" />
+        <input type="hidden" name="chiro" value="Jokonta" />
+        <input type="text" name="naam" />
+        <input type="email" name="email" />
+        <input type="tel" name="telefoon" />
+        <input type="text" name="onderwerp" />
+        <textarea name="bericht"></textarea>
+      </form>
     </>
   )
 }
